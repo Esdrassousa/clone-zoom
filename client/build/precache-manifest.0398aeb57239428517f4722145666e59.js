bbfa9312ab7ced1392e98daf298e8083d5771865
Esdras <esdrassousasoares@hotmail.com>
2021-06-19 23:32:30 -0300

    update

---
+++ client/build/precache-manifest.0398aeb57239428517f4722145666e59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d5eeb13830f53ee037a8c91bbbf1e06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56dfac3c6d6fa80a0870",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "303d1971fb9d406e8ccd",
+    "url": "/static/js/2.2e017cfd.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.2e017cfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56dfac3c6d6fa80a0870",
+    "url": "/static/js/main.8c003633.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
